09e34cd455e51489db01cae789f2cfce38dc107b
Ira Horecka <ira89@icloud.com>
2021-10-31 11:13:39 -0400

    modularize nutrient_assessment/main,py

--- nutrient_assessment/dataproc/plot.py
+++
@@ -1,5 +0,0 @@
-import seaborn as sns
-
-sns.set_theme()
-
-# down_regulated = sns.clustermap(condensed.fillna(0).T, cmap='rocket_r')
--- nutrient_assessment/main.py
+++ nutrient_assessment/main.py
@@ -1,33 +1,32 @@
 import os
 
+import seaborn as sns
+
 from dataproc import read_csv
 import dataproc.untargeted_ms as ms
 
+# TODO: KEEP METABOLITES WHOSE CONTROL CV < 15%
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 DATA_PATH = os.path.join(BASE_PATH, "data")
+sns.set_theme()
 
-if __name__ == "__main__":
-    untargeted_yeast_ms_path = os.path.join(
-        DATA_PATH, "exportFile_irahorecka_yeast_nutrient_array_150milliminute_retention_time.csv"
-    )
-    untargeted_yeast_ms_df = read_csv(untargeted_yeast_ms_path)
-    untargeted_yeast_ms_df = ms.get_df_with_cols_to_keep(
-        untargeted_yeast_ms_df, col_substrings=["Compound Name", "Area"]
-    )
-    untargeted_yeast_ms_df = ms.transpose_and_reset_idx(untargeted_yeast_ms_df)
-    untargeted_yeast_ms_df = ms.mv_row_as_header(untargeted_yeast_ms_df, row_idx=0)
 
+def isolate_cols_and_transpose_df(df):
+    df = ms.get_df_with_cols_to_keep(df, col_substrings=["Compound Name", "Area"])
+    df = ms.transpose_and_reset_idx(df)
+    return ms.mv_row_as_header(df, row_idx=0)
+
+
+def mutate_and_relabel_nutrient_data(df):
     ternary_exp = [
-        untargeted_yeast_ms_df["Compound Name"].str.split("_").str[1].str[-1].str.isdigit(),
-        untargeted_yeast_ms_df["Compound Name"].str.split("_").str[1].str[-1],
-        untargeted_yeast_ms_df["Compound Name"].str.split("_").str[1],
+        df["Compound Name"].str.split("_").str[1].str[-1].str.isdigit(),
+        df["Compound Name"].str.split("_").str[1].str[-1],
+        df["Compound Name"].str.split("_").str[1],
     ]
-    untargeted_yeast_ms_df = ms.map_ternary_exp_and_replace_values(
-        untargeted_yeast_ms_df, ternary_exp, new_colname="Sample Group"
-    )
+    df = ms.map_ternary_exp_and_replace_values(df, ternary_exp, new_colname="Sample Group")
     # Drop useless Compound Name column, as we have our Sample Group column
-    untargeted_yeast_ms_df = untargeted_yeast_ms_df.drop(columns="Compound Name")
+    df = df.drop(columns="Compound Name")
 
     group_id_to_nutrient = {
         "1": "GLC | ASP",
@@ -37,44 +36,66 @@ if __name__ == "__main__":
         "5": "GAL | GLN",
         "6": "GAL | AMN",
     }
-    untargeted_yeast_ms_df = ms.map_dict_and_replace_values(
-        untargeted_yeast_ms_df, colname="Sample Group", dict_map=group_id_to_nutrient
-    )
-    untargeted_yeast_ms_df = ms.convert_to_numerics(untargeted_yeast_ms_df)
+    return ms.map_dict_and_replace_values(df, colname="Sample Group", dict_map=group_id_to_nutrient)
 
+
+def aggregate_mean_std_cv_from_nutrient_data(df):
+    df = ms.convert_to_numerics(df)
     # Begin aggregation
-    untargeted_yeast_ms_df_mean = ms.group_and_agg(
-        untargeted_yeast_ms_df, colname="Sample Group", agg_type="mean"
-    )
-    untargeted_yeast_ms_df_std = ms.group_and_agg(
-        untargeted_yeast_ms_df, colname="Sample Group", agg_type="std"
-    )
-    untargeted_yeast_ms_df_cv = untargeted_yeast_ms_df_std.div(
-        untargeted_yeast_ms_df_mean
-    ).reset_index()
+    df_mean = ms.group_and_agg(df, colname="Sample Group", agg_type="mean")
+    df_std = ms.group_and_agg(df, colname="Sample Group", agg_type="std")
+    df_cv = ms.convert_to_numerics(df_std.div(df_mean).reset_index())
+    return df_mean, df_std, df_cv
 
+
+def filter_mean_data_from_control_cv_threshold(df_mean, df_cv, cv_threshold=0.15):
+    # Non-numeric column - pop and reintroduce later
+    sample_group = df_cv.pop("Sample Group")
+    df_cv_adj = df_cv[df_cv < cv_threshold]
+    # 5 is the index of our control
+    df_cv_adj = df_cv_adj.loc[:, ~df_cv_adj.iloc[5].isna()]
+    df_cv_adj["Sample Group"] = sample_group
+    df_cv_adj = df_cv_adj.set_index("Sample Group")
+    return df_mean[list(df_cv_adj)]
+
+
+def normalize_nutrient_data_to_control(df):
     # Remove blank row and CTRL
-    untargeted_yeast_ms_df_mean = untargeted_yeast_ms_df_mean.drop(index=["Blank", "CTRL"])
-    # Divide groups 1-6 by group 3 (i.e. our control) to get relative expression diff
-    untargeted_yeast_ms_df_mean.loc[
-        :, "2_2_Dimethylsuccinic_acid":
-    ] = untargeted_yeast_ms_df_mean.loc[:, "2_2_Dimethylsuccinic_acid":].div(
-        untargeted_yeast_ms_df_mean.iloc[3]["2_2_Dimethylsuccinic_acid":]
+    df = df.drop(index=["Blank", "CTRL"])
+    # Divide groups 1-6 by group 3 (i.e., our control) to get relative expression diff
+    df.loc[:, "2_2_Dimethylsuccinic_acid":] = df.loc[:, "2_2_Dimethylsuccinic_acid":].div(
+        df.iloc[3]["2_2_Dimethylsuccinic_acid":]
     )
-    # Re-apply float data type
-    untargeted_yeast_ms_df_mean = untargeted_yeast_ms_df_mean.drop(index="GLC | AMN")
-    untargeted_yeast_ms_df_mean = untargeted_yeast_ms_df_mean.astype(float)
     # Drop control row (Glucose and ammonia)
+    df = df.drop(index="GLC | AMN")
+    # Re-apply float data type
+    return df.astype(float)
 
-    untargeted_yeast_ms_df_mean_log2 = ms.get_log2_df(
-        untargeted_yeast_ms_df_mean, downregulated=False, log2_weight=1
+
+if __name__ == "__main__":
+    # Get pandas df from CSV path
+    untargeted_yeast_ms_path = os.path.join(
+        DATA_PATH, "exportFile_irahorecka_yeast_nutrient_array_150milliminute_retention_time.csv"
+    )
+    untargeted_yeast_ms_df = read_csv(untargeted_yeast_ms_path)
+    # Manipulate df for aggregation
+    untargeted_yeast_ms_df = isolate_cols_and_transpose_df(untargeted_yeast_ms_df)
+    untargeted_yeast_ms_df = mutate_and_relabel_nutrient_data(untargeted_yeast_ms_df)
+
+    # Aggregate df and find normalized values in respect to the control
+    agg_nutrient_mean, agg_nutrient_std, agg_nutrient_cv = aggregate_mean_std_cv_from_nutrient_data(
+        untargeted_yeast_ms_df
+    )
+    # agg_nutrient_mean = filter_mean_data_from_control_cv_threshold(
+    #     agg_nutrient_mean, agg_nutrient_cv, cv_threshold=0.25
+    # )
+    norm_agg_nutrient_mean = normalize_nutrient_data_to_control(agg_nutrient_mean)
+    norm_agg_nuttient_mean_log2 = ms.get_log2_df(
+        norm_agg_nutrient_mean, downregulated=False, log2_weight=1
     )
 
-
-    import seaborn as sns
-    sns.set_theme()
-
+    # Plot results as a hierarchical dendrogram
     down_regulated = sns.clustermap(
-        untargeted_yeast_ms_df_mean_log2.fillna(0).T, cmap="rocket_r", z_score=0
+        norm_agg_nuttient_mean_log2.fillna(0).T, cmap="rocket_r", z_score=0
     )
     down_regulated.savefig("test_zscore.eps", format="eps")
